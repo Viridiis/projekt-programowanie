e1887bf5752ae0c571919c1cc2458a461d84fc01
Viridiis <95989669+Viridiis@users.noreply.github.com>
2023-02-14 23:37:59 +0100

    Update zadanie1.py

--- zadanie1.py
+++ zadanie1.py
@@ -1,10 +1,10 @@
-a = int(input("podaj pierwsza liczbe: "))
+z = int(input("podaj pierwsza liczbe: "))
 b = int(input("podaj druga liczbe: "))
-if (a < 0):
+if (z < 0):
 	print("pierwsza liczba musi byc dodatnia!")
 if (b < 0):
 	print("druga liczba musi byc dodatnia!")
 	
 for c in range(1, m):
 	if c % b == 0:
-		print(a)
+		print(z)
