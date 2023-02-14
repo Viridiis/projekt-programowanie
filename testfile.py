781696de080f283b2cb415c0e4354a5fb6093dd4
Viper4K <K44Viper@yahoo.com>
2023-02-14 14:58:04 +0100

    pierwsza wersja kodu programu z podstaw programowania

---
+++ testfile.py
@@ -0,0 +1,20 @@
+a = int(input("podaj pierwsza liczbe: "))
+b = int(input("podaj druga liczbe: "))
+if (a < 0):
+	print("pierwsza liczba musi byc dodatnia!")
+if (b < 0):
+	print("druga liczba musi byc dodatnia!")
+	
+for c in range(1, m):
+	if c % b == 0:
+		print(a)
+
+#edit
+#d = int(input("podaj pierwsza liczbe: "))
+#if (d < 0):
+#	print("pierwsza liczba musi byc dodatnia!")
+#    
+#e = 1
+#for f in range(2, d+1)
+#    e *= f
+#    print(f)
