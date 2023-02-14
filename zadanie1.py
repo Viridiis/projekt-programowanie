63c880bb7c3a53c4f8653b5490445ae88fdb0ac9
Viper4K <K44Viper@yahoo.com>
2023-02-14 15:46:18 +0100

    Zmiana podział pliku testfile.py na dwa pliki z zadaniami, zadanie1.py i zadanie2.py

--- zadanie1.py
+++ zadanie1.py
@@ -8,13 +8,3 @@ if (b < 0):
 for c in range(1, m):
 	if c % b == 0:
 		print(a)
-
-#edit
-#d = int(input("podaj pierwsza liczbe: "))
-#if (d < 0):
-#	print("pierwsza liczba musi byc dodatnia!")
-#    
-#e = 1
-#for f in range(2, d+1)
-#    e *= f
-#    print(f)
---
+++ zadanie2.py
@@ -0,0 +1,8 @@
+d = int(input("podaj pierwsza liczbe: "))
+if (d < 0):
+	print("pierwsza liczba musi byc dodatnia!")
+    
+e = 1
+for f in range(2, d+1)
+    e *= f
+    print(f)
